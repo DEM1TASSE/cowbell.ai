3b55ca941274a6fe2a3532072335b5ed67e3811d
neverSettles <christopherRSettles@gmail.com>
2023-08-05 16:40:32 -0700

    Make tempo detection better

--- tempo.py
+++ tempo.py
@@ -1,20 +1,23 @@
 import librosa
+import numpy as np
+
 
 def detect_tempo(file_name):
     # Load the mp3 file
     y, sr = librosa.load(file_name)
 
-    # Use librosa's beat tracker to get the tempo and beat frames
-    tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+    # Compute the spectral contrast
+    S = np.abs(librosa.stft(y))
+    contrast = librosa.feature.spectral_contrast(S=S, sr=sr)
+
+    # Detect the onset strength envelope using spectral contrast
+    onset_env = librosa.onset.onset_strength(S=contrast, sr=sr)
+
+    # Use librosa's beat tracker to get the tempo and beat frames from the onset envelope
+    tempo, beat_frames = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
 
     print(f"The detected tempo of the file '{file_name}' is: {tempo} BPM")
 
-    print(f"Saving the beat frames to '{file_name}_beat_frames.txt'")
-    # Save the beat frames to a text file
-    with open(f'{file_name}_beat_frames.txt', 'w') as f:
-        for i, beat_frame in enumerate(beat_frames):
-            f.write(f'{beat_frame}_{i}\n')
-            
 
 # You can use this function to detect the tempo of an mp3 file
 # Just replace 'your_file.mp3' with the path to the mp3 file you want to analyze
