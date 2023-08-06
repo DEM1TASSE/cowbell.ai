39a0d9e5465ee69ab5b46f56f4014d739cfeaf62
neverSettles <christopherRSettles@gmail.com>
2023-08-06 15:11:07 -0700

    Update combine to add cowbell at optimum time

--- combine.py
+++ combine.py
@@ -5,7 +5,9 @@ import numpy as np
 from pytube import YouTube
 from pydub import AudioSegment
 
-url = "https://www.youtube.com/watch?v=cimoNqiulUE"
+import msaf
+
+url = "https://www.youtube.com/watch?v=-MsvER1dpjM"
 yt = YouTube(url)
 
 audio = yt.streams.get_audio_only()
@@ -20,23 +22,26 @@ AudioSegment.from_file(webm_file).export(mp3_file, format="mp3")
 
 print("Download and conversion completed!!")
 
-def combine_audio_with_tempo(main_audio_path, short_audio_path, tempo):
+def combine_audio_during_interval(main_audio_path, cowbell_audio_path, tempo, start_time, end_time):
+    # Load both audio files
     main_audio = AudioSegment.from_mp3(main_audio_path)
-    short_audio = AudioSegment.from_mp3(short_audio_path)
+    cowbell_audio = AudioSegment.from_mp3(cowbell_audio_path)
 
     # Compute the total play time
     main_audio_length = len(main_audio)
 
+    # Convert start and end times to milliseconds
+    start_time_ms = start_time * 1000
+    end_time_ms = end_time * 1000
+
     # Calculate the interval between each beat (in milliseconds)
     interval = 60 / tempo * 1000  # Convert to milliseconds
 
-    # Add the shorter audio at each beat
+    # Overlay the cowbell audio during the specified interval
     combined_audio = main_audio
-    beat_counter = 0
     for i in np.arange(0, main_audio_length, interval):
-        if beat_counter % 2 == 0 or True:  # Play the short audio only on even beats
-            combined_audio = combined_audio.overlay(short_audio, position=int(i))
-        beat_counter += 1
+        if start_time_ms <= i <= end_time_ms:  # Check if the current position is within the specified time range
+            combined_audio = combined_audio.overlay(cowbell_audio, position=int(i))
 
     return combined_audio
 
@@ -70,16 +75,47 @@ def detect_tempo(file_name):
     print(f"The detected tempo of the file '{file_name}' is: {tempo} BPM")
     return int(tempo)
 
+# Extract the code into a function:
+def get_segments(audio_file):
+    # 2. Segment the file using the default MSAF parameters (this might take a few seconds)
+    boundaries, labels = msaf.process(audio_file, feature='mfcc',
+                                      boundaries_id = 'cnmf',plot=False)
+    print('feature: mfcc')
+    print('Estimated boundaries:', boundaries)
+    print('Estimated labels:', labels)
+
+    # 3. Save segments using the MIREX format
+    out_file = 'segments.txt'
+    print('Saving output to %s' % out_file)
+    msaf.io.write_mirex(boundaries, labels, out_file)
+    return boundaries, labels
+
+# get the first segment that is longer than 10 seconds
+def get_first_segment(audio_file):
+    boundaries, labels = get_segments(audio_file)
+    for i in range(2, len(boundaries)):
+        if boundaries[i] - boundaries[i-1] > 10:
+            return boundaries[i-1], boundaries[i], i
+
+
 # Load the audio files
 main_audio_path = mp3_file
 short_audio_path = 'cowbell.mp3'
 tempo = detect_tempo(mp3_file)  # Play the short audio at 120 BPM
 
+best_seg_start, best_seg_end, i = get_first_segment(mp3_file)
+
+print(best_seg_start, best_seg_end, i)
+
+# Set the interval during which the cowbell should play (in seconds)
+start_time = best_seg_start  # Start after 1 minute
+end_time = best_seg_end   # End after 2 minutes
+
 # Combine the audio files
-combined_audio = combine_audio_with_tempo(main_audio_path, short_audio_path, tempo)
+combined_audio = combine_audio_during_interval(main_audio_path, short_audio_path, tempo, start_time, end_time)
 
 # Play the combined audio
 # play_audio(combined_audio)
 # Save the combined audio to an MP3 file
 combined_audio.export(mp3_file[:-4] + '_combined.mp3', format="mp3")
-# play_audio(combined_audio)
+play_audio(combined_audio)
